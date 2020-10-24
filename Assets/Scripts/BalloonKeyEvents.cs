a5827a705e564eb4b87adc34d0f0686ab30a3ffe
DustinJoosen <dustinjoosen2003@gmail.com>
2020-10-24 16:44:02 +0200

    Use delegates to clean up key events

--- Assets/Scripts/BalloonKeyEvents.cs
+++ Assets/Scripts/BalloonKeyEvents.cs
@@ -1,119 +1,77 @@
 ﻿using Assets.Scripts.Helpers;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BalloonKeyEvents : MonoBehaviour
 {
-    public GameObject Balloon;
-    public float MetersPerFrame = 0.5f;
+    public float metersPerFrame = 0.5f;
+    public static float genericMetersPerFrame;
 
-    private float X;
-    private float Y;
-    private float Z;
+    public float shiftSpeed = 3.0f;
 
-    private bool IsPressingLeft = false;
-    private bool IsPressingRight = false;
-    private bool IsPressingUp = false;
-    private bool IsPressingDown = false;
-    private bool IsPressingShift = false;
+    private static float x;
+    private static float y;
+    private static float z;
 
-    private bool IsCollisionHappening = false;
-    
-    private KeyCode LastKey;
-    private KeyCode SecondLastKey;
+    private static bool isPressingShift = false;
+
+    public KeyEventHandler keyEventHandler = new KeyEventHandler();
+	public delegate void KeyAction(KeyCode keyEvent);
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Started game");
+        KeyAction actionHandler = HandleMovement;
 
-        X = Balloon.transform.position.x;
-        Y = Balloon.transform.position.y;
-        Z = Balloon.transform.position.z;
+        keyEventHandler.AddEventHandler(new KeyEvent(KeyCode.LeftArrow, actionHandler));
+        keyEventHandler.AddEventHandler(new KeyEvent(KeyCode.RightArrow, actionHandler));
+        keyEventHandler.AddEventHandler(new KeyEvent(KeyCode.UpArrow, actionHandler));
+        keyEventHandler.AddEventHandler(new KeyEvent(KeyCode.DownArrow, actionHandler));
+        keyEventHandler.AddEventHandler(new KeyEvent(KeyCode.LeftShift, actionHandler));
+
+        x = this.transform.position.x;
+        y = this.transform.position.y;
+        z = this.transform.position.z;
+
+        Debug.Log("Started game");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        int key_been_pressed = 0;
-        float meters_per_frame = MetersPerFrame;
+        genericMetersPerFrame = metersPerFrame;
+        if (isPressingShift)
+            genericMetersPerFrame *= shiftSpeed;
 
-        IsPressingLeft = IsPressingKey(KeyCode.LeftArrow, IsPressingLeft);
-        IsPressingRight = IsPressingKey(KeyCode.RightArrow, IsPressingRight);
-        IsPressingUp = IsPressingKey(KeyCode.UpArrow, IsPressingUp);
-        IsPressingDown = IsPressingKey(KeyCode.DownArrow, IsPressingDown);
-        IsPressingShift = IsPressingKey(KeyCode.LeftShift, IsPressingShift);
-
-
-        if (IsPressingShift) 
-            meters_per_frame = meters_per_frame * 3;
-
-        if (IsPressingLeft)
-		{
-            if(!(IsCollisionHappening && LastKey == KeyCode.LeftArrow))
-			{
-                LastKey = KeyCode.LeftArrow;
-                X -= meters_per_frame;
-                key_been_pressed++;
-			}
-		}
-
-        if (IsPressingRight)
-		{
-            if (!(IsCollisionHappening && LastKey == KeyCode.RightArrow))
-			{
-                LastKey = KeyCode.RightArrow;
-                X += meters_per_frame;
-                key_been_pressed++;
-            }
-        }
-
-        if (IsPressingUp)
-        {
-            if (!(IsCollisionHappening && LastKey == KeyCode.UpArrow))
-            {
-                LastKey = KeyCode.UpArrow;
-                Z += meters_per_frame;
-                key_been_pressed++;
-            }
-        }
-
-        if (IsPressingDown)
-		{
-            if(!(IsCollisionHappening && LastKey == KeyCode.DownArrow))
-			{
-                LastKey = KeyCode.DownArrow;
-                Z -= meters_per_frame;
-                key_been_pressed++;
-            }
-        }
-
-        if (key_been_pressed > 1)
-            Debug.Log("Pressed multiple keys");
-
-        Balloon.transform.position = new Vector3(X, Y, Z);
+        keyEventHandler.CheckKeyEvents();
+        this.transform.position = new Vector3(x, y, z);
     }
 
-	private void OnTriggerEnter(Collider other)
+    public static void HandleMovement(KeyCode keyCode)
 	{
-        IsCollisionHappening = true;
-        Debug.Log("Balloon entered collision");
-	}
+        isPressingShift = keyCode == KeyCode.LeftShift;
 
-	private void OnTriggerExit(Collider other)
-	{
-        IsCollisionHappening = false;
-        Debug.Log("Balloon left the collision");
-	}
+        switch (keyCode)
+		{
+            case KeyCode.LeftArrow:
+                x -= genericMetersPerFrame;
+                break;
+            case KeyCode.RightArrow:
+                x += genericMetersPerFrame;
+                break;
+            case KeyCode.UpArrow:
+                z += genericMetersPerFrame;
+                break;
+            case KeyCode.DownArrow:
+                z -= genericMetersPerFrame;
+                break;
+            default:
+                break;
+		}
 
-	private bool IsPressingKey(KeyCode keyCode, bool IsPressing)
-	{
-        IsPressing = Input.GetKeyDown(keyCode) ? true : IsPressing;
-        IsPressing = Input.GetKeyUp(keyCode) ? false : IsPressing;
-
-        return IsPressing;
 	}
 
 }
---
+++ Assets/Scripts/Helpers/KeyEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Helpers
+{
+	public class KeyEvent
+	{
+		public KeyCode Key { get; set; }
+		public Delegate Action { get; set; }
+		
+		private bool isPressing = false;
+
+		public KeyEvent(KeyCode key, Delegate action)
+		{
+			this.Key = key;
+			this.Action = action;
+			this.isPressing = false;
+		}
+		
+		public bool CheckIfPressing()
+		{
+			bool pressing = this.isPressing;
+
+			pressing = Input.GetKeyDown(this.Key) ? true : pressing;
+			pressing = Input.GetKeyUp(this.Key) ? false : pressing;
+
+			isPressing = pressing;
+			return pressing;
+		}
+	}
+}
