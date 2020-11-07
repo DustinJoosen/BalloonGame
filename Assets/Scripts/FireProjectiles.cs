9ed343ddbec44a166d2381f363a3f331b9811436
DustinJoosen <dustinjoosen2003@gmail.com>
2020-11-07 14:53:30 +0100

    shooting basic bullets

--- Assets/Scripts/FireProjectiles.cs
+++ Assets/Scripts/FireProjectiles.cs
@@ -5,55 +5,25 @@ using UnityEngine;
 
 public class FireProjectiles : MonoBehaviour
 {
-    public GameObject ProjectileOrigin;
-    public GameObject ProjectilePrefab;
-    public float ForwardForce = 0.0f;
 
-    private Vector3 originPosition;
-    private Quaternion originRotation;
-
-    private Rigidbody projectileRigidBody;
-    private List<Rigidbody> projectiles;
+    public GameObject Projectile;
+    public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        originPosition = ProjectileOrigin.transform.position;
-        originRotation = ProjectileOrigin.transform.rotation;
-
-        projectileRigidBody = ProjectilePrefab.GetComponent<Rigidbody>();
-        projectiles = new List<Rigidbody>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-		{
-            ProjectilePrefab.transform.position = new Vector3(ProjectilePrefab.transform.position.x + ForwardForce, ProjectilePrefab.transform.position.y, ProjectilePrefab.transform.position.z);
-		}
-
-        return;
-
-        originPosition = ProjectileOrigin.transform.position;
-        originRotation = ProjectileOrigin.transform.rotation;
-
         if (Input.GetKeyDown(KeyCode.Space))
 		{
-            Rigidbody projectileClone = Instantiate(projectileRigidBody, originPosition, originRotation) as Rigidbody;
-            projectileClone.AddForce(transform.forward * ForwardForce);
-        }
-        
-    }
+            Debug.Log("Shooting new Bullet");
+            GameObject instBullet = Instantiate(Projectile, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidbody.AddForce(Vector3.forward * speed);
+		}
 
-
-    private Rigidbody Shoot()
-	{
-        Rigidbody projectileClone = Instantiate(projectileRigidBody, originPosition, originRotation) as Rigidbody;
-        projectileClone.AddForce(transform.forward * ForwardForce);
-        //projectileClone.velocity = transform.forward * ForwardForce;
-
-        return projectileClone;
 	}
 }
