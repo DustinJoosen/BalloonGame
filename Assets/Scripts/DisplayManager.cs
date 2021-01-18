825cb30fbee7ee280558a2dc8b0e293489693d37
DustinJoosen <dustinjoosen2003@gmail.com>
2021-01-18 23:50:51 +0100

    tried display managing, also used playerprefs

---
+++ Assets/Scripts/DisplayManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DisplayManager : MonoBehaviour, IPointerClickHandler
+{
+    //TP -> Top perspective
+    //FP -> First person
+    private bool IsFirstPerson
+	{
+        get => PlayerPrefs.GetString("Camera") == "FP";
+        set => PlayerPrefs.SetString("Camera", value ? "FP" : "TP");
+	}
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsFirstPerson = PlayerPrefs.GetString("Camera") == "FP";
+    }
+
+	public void SetFirstPersonCamera()
+	{
+        if (IsFirstPerson)
+            return;
+
+        IsFirstPerson = true;
+        Debug.Log("Trigger display changes");
+    }
+
+    public void SetTopPerspectiveCamera()
+	{
+        if (!IsFirstPerson)
+            return;
+
+        IsFirstPerson = false;
+        Debug.Log("Trigger display changes");
+    }
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+        Debug.Log("pointer clicked");
+	}
+}
